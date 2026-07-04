55bfcd878c5c563867d45cc44f7a10c303855a02
Pavle MacBook Air <pavlemilovanovic42@gmail.com>
2026-07-05 00:20:05 +0200

    Loading and parsing cards.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import { readFileSync } from "fs";
+
+const cardsFile = readFileSync("cards.json", "utf8");
+const cards = JSON.parse(cardsFile);
 const cli_arguments = process.argv.slice(2);
 
 for (const a of cli_arguments) {
     console.log(a);
 }
+
+console.log(cards);
